7a47c5e31bbb4b75288903e45d8047e7e2cc2c94
YakozenG <hamiy1016@gmail.com>
2021-06-24 23:45:57 +0900

    save

--- test/test.py
+++ test/test.py
@@ -1,1 +1,18 @@
-print('start')
+
+BASE_AREA = [500, 600]
+GAP_SETTING = {'top': [5, 7.5], 'bottom': [
+    7, 10], 'left': [0, 3], 'right': [0, 3]}
+SLIT = 1.6
+SIZE = {'width': 80, 'Height': 50}
+
+
+def main():
+    pass
+
+
+def get_data(base_area, gap_list, size):
+    pass
+
+
+if __name__ == '__main__':
+    main()
